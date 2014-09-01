f0ac7538c74c5e675e6be5663dffbf50b438c760
Sara El Nahal <saraelnahal@Saras-MacBook-Pro-4.local>
2014-09-01 14:25:31 +0200

    slider

--- application/views/slider.php
+++ application/views/slider.php
@@ -1,22 +1,64 @@
 <div class="hidden-sm hidden-xs slider">
             <ul class="bxslider" style="height: 455px!important;">
                   <li>
-                      <img src="<?php echo base_url(); ?>application/static/images/sample_banner.png" style="">
+                      <a href="<?php echo base_url(); ?>viewAllProperties"><img class="slider_imgs" src="<?php echo base_url(); ?>application/static/images/sliderimg.png"></a>
+                      <div class="slider_logo">
+                          <img src="<?php echo base_url(); ?>application/static/images/sliderlogo.png">
+                      </div>
+                      <div class="slider_components">
+                          <p>NOW HALF PRICE</p>
+                          <p>AND PAYMENT ON 6 YEARS</p>
+                      </div>
                   </li>
                   <li>
-                      <img src="<?php echo base_url(); ?>application/static/images/sample_banner2.png" style="">
+                      <a href="<?php echo base_url(); ?>viewAllProperties"><img class="slider_imgs" src="<?php echo base_url(); ?>application/static/images/sliderimg.png"></a>
+                      <div class="slider_logo">
+                          <img src="<?php echo base_url(); ?>application/static/images/sliderlogo.png">
+                      </div>
+                      <div class="slider_components">
+                          <p>NOW HALF PRICE</p>
+                          <p>AND PAYMENT ON 6 YEARS</p>
+                      </div>
                   </li>
                   <li>
-                      <img src="<?php echo base_url(); ?>application/static/images/sample_banner.png" style="">
+                      <a href="<?php echo base_url(); ?>viewAllProperties"><img class="slider_imgs" src="<?php echo base_url(); ?>application/static/images/sliderimg.png"></a>
+                      <div class="slider_logo">
+                          <img src="<?php echo base_url(); ?>application/static/images/sliderlogo.png">
+                      </div>
+                      <div class="slider_components">
+                          <p>NOW HALF PRICE</p>
+                          <p>AND PAYMENT ON 6 YEARS</p>
+                      </div>
                   </li>
                   <li>
-                      <img src="<?php echo base_url(); ?>application/static/images/sample_banner2.png" style="">
+                      <a href="<?php echo base_url(); ?>viewAllProperties"><img class="slider_imgs" src="<?php echo base_url(); ?>application/static/images/sliderimg.png"></a>
+                      <div class="slider_logo">
+                          <img src="<?php echo base_url(); ?>application/static/images/sliderlogo.png">
+                      </div>
+                      <div class="slider_components">
+                          <p>SPECIAL OFFER</p>
+                          <p>PAYMENT ON 6 YEARS</p>
+                      </div>
                   </li>
                   <li>
-                      <img src="<?php echo base_url(); ?>application/static/images/sample_banner.png" style="">
+                      <a href="<?php echo base_url(); ?>viewAllProperties"><img class="slider_imgs" src="<?php echo base_url(); ?>application/static/images/sliderimg.png"></a>
+                      <div class="slider_logo">
+                          <img src="<?php echo base_url(); ?>application/static/images/sliderlogo.png">
+                      </div>
+                      <div class="slider_components">
+                          <p>NOW HALF PRICE</p>
+                          <p>AND PAYMENT ON 6 YEARS</p>
+                      </div>
                   </li>
                   <li>
-                      <img src="<?php echo base_url(); ?>application/static/images/sample_banner2.png" style="">
+                      <a href="<?php echo base_url(); ?>viewAllProperties"><img class="slider_imgs" src="<?php echo base_url(); ?>application/static/images/sliderimg.png"></a>
+                      <div class="slider_logo">
+                          <img src="<?php echo base_url(); ?>application/static/images/sliderlogo.png">
+                      </div>
+                      <div class="slider_components">
+                          <p>NOW HALF PRICE</p>
+                          <p>AND PAYMENT ON 6 YEARS</p>
+                      </div>
                   </li>
               </ul>
         </div>
