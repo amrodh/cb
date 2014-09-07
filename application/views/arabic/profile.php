6734bc845225ab6f58269d115cfed010daf8ae28
Sara El Nahal <saraelnahal@Saras-MacBook-Pro-4.local>
2014-09-07 13:42:27 +0200

    nothing

---
+++ application/views/arabic/profile.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+    <body>
+        <?php include('header-ar.php'); ?>
+        <div class="container shareproperty_main_div">
+            <div class="shareproperty_top_div">
+                الملف الشخصي
+                <div style="font-size: 12px;font-weight: normal;">
+                    
+                </div>
+            </div>
+            <div id="properties_bottom_div">
+                <div class="container" id="profile_bottom_inner_div" style="">
+                    <div class="row">
+                        <div class="col-lg-2 profile_titles">
+                            إسم المستخدم:
+                        </div>
+                        <div class="col-lg-10 profile_content">
+                            a.ezzat
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-2 profile_titles">
+                            البريد الالكتروني:
+                        </div>
+                        <div class="col-lg-10 profile_content">
+                            amiramezzat@gmail.com
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-2 profile_titles">
+                            العنوان:
+                        </div>
+                        <div class="col-lg-10 profile_content">
+                            20 Degla St., Mohandesin, Giza
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-2 profile_titles">
+                            رقم الهاتف:
+                        </div>
+                        <div class="col-lg-10 profile_content">
+                            01234567890
+                        </div>
+                    </div>
+                    <div class="row" style="width: 13%; margin: auto; margin-top: 20px;">
+                        <button type="submit" class="btn btn-default profile_btn_submit">عدل</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php include('footer.php'); ?>
+    </body>
+</html>
