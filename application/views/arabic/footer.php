9a4b88244344bf6ee0f4fbc763aedb33412f6a8b
Sara El Nahal <saraelnahal@Saras-MacBook-Pro-5.local>
2015-02-19 17:59:30 +0200

    analytics

--- application/views/arabic/footer.php
+++ application/views/arabic/footer.php
@@ -185,7 +185,16 @@
          </script>
 <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&callback=initialize"></script>
          <script type="text/javascript" src="<?= base_url(); ?>application/static/js/script.js"></script>
-
+<script>
+              (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+              (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+              })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+             
+              ga('create', 'UA-49598488-15', 'auto');
+              ga('send', 'pageview');
+         
+        </script>
 </body>
 </html>
 
